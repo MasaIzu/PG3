7388c6de2e3c03b69471a32f4e1573632cc949f0
masato izumida <Oioi_0126@outlook.com>
2022-11-30 22:37:47 +0900

    駅リスト

--- main.cpp
+++ main.cpp
@@ -1,14 +1,72 @@
-#include<stdlib.h>
-#include<list>
+#include <stdio.h>
+#include <list>
+#include <iostream>
 
-using namespace std;
+int main()
+{
+	const char* yamanoteStationName[28] = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"ShinOkubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
 
-int main() {
+	//ƒŠƒXƒg‚ğ¶¬
+	std::list<const char*> lst(yamanoteStationName, std::end(yamanoteStationName));
 
-	list<const char*> lst{"Oosaki","Sinagawa","Tamachi","Hamamastutyou","Sinbasi","Yuurakutyou","Tokyo","Kanda","Akihabara",};
+	//‰wˆê——
+	printf("1970”N‰wˆê——\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+	//ˆê‚ÂŠJ‚¯‚é
+	printf("\n");
 
-	
+	//‰wˆê——
+	printf("2019”N‰wˆê——\n");
+	std::list<const char*>::iterator itr1971 = std::next(lst.begin(), 7);
+	lst.insert(itr1971, "NishiniNippori");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+	//ˆê‚ÂŠJ‚¯‚é
+	printf("\n");
+
+	//‰wˆê——
+	printf("2022”N‰wˆê——\n");
+	std::list<const char*>::iterator itr2019 = std::next(lst.begin(), 25);
+	lst.insert(itr2019, "TakanawaGateway");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
 
 	return 0;
-
 }

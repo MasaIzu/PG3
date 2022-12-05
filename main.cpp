3625b1c1b083670aa3300604ca6b8eea15701d02
masato izumida <Oioi_0126@outlook.com>
2022-12-05 12:29:09 +0900

    03_01できた

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-#include<stdlib.h>
-#include"TestSingleton.h"
-
+#include <stdio.h>
+#include <Windows.h>
+#include "SceneManager.h"
 int main() {
+	
+	SceneManager* sceneManager = SceneManager::GetInstance();
+	int NowScene = 0;
 
-	//インスタンスを取得して、すぐ使用
-	TestSingleton::GetInstance();
+	while (1) {
+		if (NowScene > 3) {
+			NowScene = 0;
+		}
 
+		sceneManager->ChangeScene(NowScene);
+		NowScene++;
+		Sleep(1 * 1000);
+	}
+
+	system("pause");
 	return 0;
+
 }
